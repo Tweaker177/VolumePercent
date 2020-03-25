be605ce25b32e6296669a09c915a9d1318f05465
i0s_tweak3r <djvs23@gmail.com>
2020-03-25 03:32:24 -0400

    Update vppRootListController.h

--- volumepercentprefs/vppRootListController.h
+++ volumepercentprefs/vppRootListController.h
@@ -1,5 +1,15 @@
 #import <Preferences/PSListController.h>
+#import <Preferences/PSTableCell.h>
+#import <Preferences/PSSpecifier.h>
+#import <spawn.h>
 
 @interface vppRootListController : PSListController
+
+-(id)getPreferenceSpecifier:(PSSpecifier*)specifier;
+-(void)setPreferenceValue:(id)value specifier:(PSSpecifier*)specifier;
+-(void)apply;
+-(void)respring:(id)sender;
+-(void)donationLink;
+-(void)sourceLink;
 
 @end
